5376616975ade1b84de4d2e8e88feb5aed22d8ef
BuildTools <unconfigured@null.spigotmc.org>
2018-06-18 21:55:24 +0200

    Form when adding record works for first three columns. Change of size of "home" element is requred.

--- JS.js
+++ JS.js
@@ -1,4 +1,6 @@
-﻿window.onload = function ()
+﻿var recordCount = 0;
+
+window.onload = function ()
 {
 	var addRecord = document.getElementById("addRecord");
 	
@@ -10,21 +12,24 @@
 	
 	function newRecord(evt){
 		evt.preventDefault();
+		recordCount++;
 		document.getElementById("form").style.display = "none";
+		document.getElementById("home").style.height = ((recordCount * 52) + 60) + "px";
+		
 		var records = document.getElementById("data");
 		var rw = document.createElement("tr");
 		var date = document.createElement("td");
-		var dateText = document.createTextNode(document.getElementById("form1").innerHTML);//document.getElementById("form1").innerHTML
+		var dateText = document.createTextNode(document.getElementById("form1").value);
 		var subject = document.createElement("td");
-		var subjectText = document.createTextNode(document.getElementById("form2").innerHTML);//document.getElementById("form2").innerHTML
+		var subjectText = document.createTextNode(document.getElementById("form2").value);
 		var description = document.createElement("td");
-		var descriptionText = document.createTextNode(document.getElementById("form3").innerHTML);//document.getElementById("form3").innerHTML
+		var descriptionText = document.createTextNode(document.getElementById("form3").value);
 		var author = document.createElement("td");
 		var authorText = document.createTextNode("author");
 		var dateOfAdding = document.createElement("td");
 		var dateOfAddingText = document.createTextNode("adding");
 		var action = document.createElement("td");
-		var actionText = document.createTextNode("Upvote Downvote Edit");
+		var actionText = document.createTextNode("Edit");
 	
 		date.appendChild(dateText);
 		subject.appendChild(subjectText);
@@ -32,6 +37,13 @@
 		author.appendChild(authorText);
 		dateOfAdding.appendChild(dateOfAddingText);
 		action.appendChild(actionText);
+		
+		date.id = "column1";
+		subject.id = "column2";
+		description.id = "column3";
+		author.id = "column4";
+		dateOfAdding.id = "column5";
+		action.id = "column6";
 		
 		rw.appendChild(date);
 		rw.appendChild(subject);
