238aeeef25749ff3aa2a7c0b5d2ad40023508ab7
BuildTools <unconfigured@null.spigotmc.org>
2018-06-18 19:43:30 +0200

    Page doesn´t reload now and new rows are kept in it.

--- JS.js
+++ JS.js
@@ -1,44 +1,44 @@
-﻿window.onload = function (){
+﻿window.onload = function ()
+{
 	var addRecord = document.getElementById("addRecord");
-	var formSubmit = document.getElementById("formSubmit");
 	
-    addRecord.onclick = function addRecord(){
+	document.getElementById("addRecord").onclick = function addRecord()
+	{
 		document.getElementById("form").style.display = "block";
 	}
 }
-function newRecord(){
-	alert("Success!");
-	document.getElementById("form").style.display = "none";
 	
-	let records = document.getElementById("data");
-	let rw = document.createElement("tr");
-	let date = document.createElement("td");
-	let dateText = document.createTextNode("0.0.0000");//document.getElementById("form1").innerHTML
-	let subject = document.createElement("td");
-	let subjectText = document.createTextNode("VV");//document.getElementById("form2").innerHTML
-	let description = document.createElement("td");
-	let descriptionText = document.createTextNode("Desc");//document.getElementById("form3").innerHTML
-	let author = document.createElement("td");
-	let authorText = document.createTextNode("author");
-	let dateOfAdding = document.createElement("td");
-	let dateOfAddingText = document.createTextNode("adding");
-	let action = document.createElement("td");
-	let actionText = document.createTextNode("Upvote Downvote Edit");
-
-	date.appendChild(dateText);
-	subject.appendChild(subjectText);
-	description.appendChild(descriptionText);
-	author.appendChild(authorText);
-	dateOfAdding.appendChild(dateOfAddingText);
-	action.appendChild(actionText);
+	function newRecord(evt){
+		evt.preventDefault();
+		document.getElementById("form").style.display = "none";
+		var records = document.getElementById("data");
+		var rw = document.createElement("tr");
+		var date = document.createElement("td");
+		var dateText = document.createTextNode(document.getElementById("form1").innerHTML);//document.getElementById("form1").innerHTML
+		var subject = document.createElement("td");
+		var subjectText = document.createTextNode(document.getElementById("form2").innerHTML);//document.getElementById("form2").innerHTML
+		var description = document.createElement("td");
+		var descriptionText = document.createTextNode(document.getElementById("form3").innerHTML);//document.getElementById("form3").innerHTML
+		var author = document.createElement("td");
+		var authorText = document.createTextNode("author");
+		var dateOfAdding = document.createElement("td");
+		var dateOfAddingText = document.createTextNode("adding");
+		var action = document.createElement("td");
+		var actionText = document.createTextNode("Upvote Downvote Edit");
 	
-	rw.appendChild(date);
-	rw.appendChild(subject);
-	rw.appendChild(description);
-	rw.appendChild(author);
-	rw.appendChild(dateOfAdding);
-	rw.appendChild(action);
-	
-	records.tbody.appendChild(rw);
-	
+		date.appendChild(dateText);
+		subject.appendChild(subjectText);
+		description.appendChild(descriptionText);
+		author.appendChild(authorText);
+		dateOfAdding.appendChild(dateOfAddingText);
+		action.appendChild(actionText);
+		
+		rw.appendChild(date);
+		rw.appendChild(subject);
+		rw.appendChild(description);
+		rw.appendChild(author);
+		rw.appendChild(dateOfAdding);
+		rw.appendChild(action);
+		
+		records.appendChild(rw);
 	}
