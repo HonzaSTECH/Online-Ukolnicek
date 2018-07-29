145a89653f0560853c3df93db0ddaef2543adaa7
BuildTools <unconfigured@null.spigotmc.org>
2018-07-29 16:42:01 +0200

    Second phase of the previous commit. Old 'online' folder with duplicated php files has been removed.

--- online/index.php
+++
@@ -1,9 +0,0 @@
-<?php session_start(); ?>
-
-<meta charset="utf-8">
-<?php
-	echo "Vítej na seznamtestu.chytrak.cz . Pro pokračování klikni na tlačítko. <br />";
-?>
-<form action="login.php">
-	<input type=submit value="Vstoupit">
-</form>
