2a5547d3228a9b6841ca19a0175993d30a250bec
BuildTools <unconfigured@null.spigotmc.org>
2018-07-29 17:26:28 +0200

    Selecting class you want to enter now displays classes' names instead of their IDs. User can see it´s role in the class. Only 'Member' or 'Admin' is currently supported (not 'Moderator').

--- home.php
+++ home.php
@@ -1,5 +1,19 @@
 <?php session_start(); ?>
 <meta charset="utf-8">
+<style>
+	.td1{
+		width: 20px;
+		height: 20px;
+	}
+	.td2{
+		width: 100px;
+		height: 20px;
+	}
+	.td3{
+		width: 75px;
+		height: 20px;
+	}
+</style>
 <p>Jsi členem v těchto třídách:</p>
 
 <?php
@@ -14,11 +28,19 @@
 	
 	if(!$data == '0'){
 		$classes = explode('0', $data);
-		echo "<form action='list.php' method='GET'>";
+		echo "<form action='list.php' method='GET'><table>";
 		foreach ($classes as $class){
-			echo "<input type=radio name='classSelect' value='$class'>' $class'<br />";
+			$result = mysqli_query($connection, "SELECT * FROM classes WHERE id='$class'");
+			$classData = mysqli_fetch_array($result);
+			$classAdmin = $classData['admin'];
+			$classData = $classData['name'];
+			$classAdmin = ($classAdmin == $user);
+			echo "<tr><td align='center' class='td1'><input type=radio name='classSelect' value='$class'></td><td align='center' class='td2'> $classData</td><td align='center' class='td3'>";
+			if($classAdmin){echo "Administrátor";}
+			else{echo "Člen";}
+			echo "</td></tr>";
 		}
-		echo "<input type=submit value='Vstoupit'></form><br />";
+		echo "</table><input type=submit value='Vstoupit'></form>";
 	}
 	else{
 		echo "Nejsi členem žádné třídy.<br />";
