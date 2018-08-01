5b06933f7a7aa12fd24b3422891f8cdf1fc1c507
BuildTools <unconfigured@null.spigotmc.org>
2018-08-01 18:04:07 +0200

    CSS files has been connected to php script files via <link> tag. Embedded style from .php files has been moved to .css style files.

--- scripts/index.php
+++ scripts/index.php
@@ -1,6 +1,7 @@
 <?php session_start(); ?>
 
 <meta charset="utf-8">
+<link rel="stylesheet" href="../styles/index.css">
 
 <p>Vítej na seznamtestu.chytrak.cz . Pro pokračování klikni na tlačítko.</p>
 <form action="login.php">
