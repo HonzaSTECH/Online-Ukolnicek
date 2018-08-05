6131fe12705f7b580ed486e4e6458980914853f1
BuildTools <unconfigured@null.spigotmc.org>
2018-08-05 21:28:54 +0200

    login.php page has been styled using login.css file. Background picture is copyrighted and MUST BE REPLACED before first official relase.

--- scripts/login.php
+++ scripts/login.php
@@ -3,39 +3,47 @@
 	unset($_SESSION['class']);
 	unset($_SESSION['user']);
 ?>
-<meta charset="utf-8">
-<link rel="stylesheet" href="../styles/login.css">
-<form method="POST" action="login.php">
-	<input type="text" name="username" placeholder="Jméno" required><br />
-	<input type="password" name="password" placeholder="Heslo" required><br />
-	<input type="submit" name="send" value="Přihlásit se">
-</form>
-<?php
-	require_once("connect.php");
-	include 'logger.php';
-	
-	if(isset($_POST['send'])){
-		$name = $_POST['username'];
-		$pass = $_POST['password'];
-		
-		$query = "SELECT * FROM users WHERE name='$name'";
-		$result = mysqli_query($connection, $query);
-		
-		$data = mysqli_fetch_array($result);
-		
-		if($data['name'] == $name){
-			if($data['password'] == $pass){
-				echo "Byl/a jsi úspěšně přihlášen/a.";
-				$_SESSION['user']=$name;
-				$ip = $_SERVER['REMOTE_ADDR'];
-				fileLog("Uživatel $name se přihlásil z IP adresy $ip");
-				echo "<script type='text/javascript'>location.href = 'home.php';</script>";
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="../styles/login.css">
+</head>
+<body>
+        <div id="loginBox">
+		<form method="POST" action="login.php" id="loginForm">
+			<input type=text name="username" placeholder="Jméno" id="usernameInput" required>
+			<br />
+			<input type=password name="password" placeholder="Heslo" id="passwordInput" required>
+			<br />
+			<input type=submit name="send" value="Přihlásit se" id="submitButton">
+		</form>
+		<div id="registerLink">Ještě nemáš účet? Zaregistruj se <a href="register.php"><u>zde</u></a>.</div>
+		<?php
+			require_once("connect.php");
+			include 'logger.php';
+			
+			if(isset($_POST['send'])){
+				$name = $_POST['username'];
+				$pass = $_POST['password'];
+				
+				$query = "SELECT * FROM users WHERE name='$name'";
+				$result = mysqli_query($connection, $query);
+				
+				$data = mysqli_fetch_array($result);
+				
+				if($data['name'] == $name){
+					if($data['password'] == $pass){
+						echo "Byl/a jsi úspěšně přihlášen/a.";
+						$_SESSION['user']=$name;
+						$ip = $_SERVER['REMOTE_ADDR'];
+						fileLog("Uživatel $name se přihlásil z IP adresy $ip");
+						echo "<script type='text/javascript'>location.href = 'home.php';</script>";
+					}
+					else{echo "<div id='loginError'>Špatné heslo</div>";}
+				}
+				else{echo "<div id='loginError'>Uživatel s tímto jménem neexistuje.</div>";}
 			}
-			else{echo "Špatné heslo";}
-		}
-		else{echo "Uživatel s tímto jménem neexistuje.";}
-	}
-	mysqli_close($connection);
+			mysqli_close($connection);
 
-?>
-<p>Ještě nemáš účet? Zaregistruj se <a href="register.php"><u>zde</u></a>.</p>
+		?>
+        </div>
+</body>
