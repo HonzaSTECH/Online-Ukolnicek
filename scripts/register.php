acbd7493e542a2015bbd99f4469bdad852ab26d2
BuildTools <unconfigured@null.spigotmc.org>
2018-08-06 19:09:20 +0200

    Register.php site has been styled using register.css file in folder styles. Background picture is same as in login site and has to be changed as well. Index.css file has been modified slightly with no visible results. .gitignore file has been updated and contains paths to testing files (scripts/server.php , scripts/tests.php , scripts/view.php).

--- scripts/register.php
+++ scripts/register.php
@@ -3,55 +3,61 @@
 	unset($_SESSION['class']);
 	unset($_SESSION['user']);
 ?>
-<meta charset="utf-8">
-<link rel="stylesheet" href="../styles/register.css">
-<form method="POST" action="register.php">
-	<input type="text" name="username" placeholder="Jméno" required><br />
-	<input type="password" name="password" placeholder="Heslo" required><br />
-	<input type="password" name="password_repeat" placeholder="Heslo znovu" required><br />
-	<input type="email" name="email" placeholder="E-mail" required><br />
-	<input type="checkbox" name="accept">Souhlasím se spracováním zadaných údajů pro účely této služby<br />
-	<input type="submit" name="send" value="Zaregistrovat se">
-</form>
-<?php
-	require_once("connect.php");
-	include 'logger.php';
-	
-	if(isset($_POST['send'])){
-		$name = $_POST['username'];
-		$pass = $_POST['password'];
-		$pass_repeat = $_POST['password_repeat'];
-		$email = $_POST['email'];
-		$accept = @$_POST['accept'];
-		
-		if(isset($accept)){
-			$query = "SELECT * FROM users WHERE name='$name'";
-			$result = mysqli_query($connection, $query);
-			$data = mysqli_fetch_array($result);
-			$data = $data['name'];
-			if(empty($data)){
-				if($pass == $pass_repeat){
-					$query = "SELECT * FROM users WHERE email = '$email'";
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="../styles/register.css">
+</head>
+<body>
+	<div id="registerBox">
+		<form method="POST" action="register.php" id="registerForm">
+			<input type="text" name="username" placeholder="Jméno" id="usernameInput" required><br />
+			<input type="password" name="password" placeholder="Heslo" id="passwordInput" required><br />
+			<input type="password" name="password_repeat" placeholder="Heslo znovu" id="repeatPasswordInput" required><br />
+			<input type="email" name="email" placeholder="E-mail" id="emailInput" required>
+			<div id="termsInput"><br /><input type="checkbox" name="accept" id="checkbox"><br /><span id=termsText>Souhlasím se spracováním zadaných údajů pro účely této služby</span></div>
+			<input type="submit" name="send" value="Zaregistrovat se" id="submitButton">
+		</form>
+		<div id="loginLink">Jsi již zaregistrován? Přihlaš se <a href="login.php"><u>zde</u></a>.</div>
+		<?php
+			require_once("connect.php");
+			include 'logger.php';
+			
+			if(isset($_POST['send'])){
+				$name = $_POST['username'];
+				$pass = $_POST['password'];
+				$pass_repeat = $_POST['password_repeat'];
+				$email = $_POST['email'];
+				$accept = @$_POST['accept'];
+				
+				if(isset($accept)){
+					$query = "SELECT * FROM users WHERE name='$name'";
 					$result = mysqli_query($connection, $query);
-					if(!empty($result) || empty($email)){
-						$query = "INSERT INTO users (name, password, email) VALUES ('$name', '$pass', '$email')";
-						$result = mysqli_query($connection, $query);
-						if($result){
-								echo "Byl/a jsi úspěšně zaregistrován/a.";
-								$_SESSION['user']=$name;
-								filelog("Uživatel $name se zaregistroval do systému.");
-								echo "<script type='text/javascript'>location.href = 'home.php';</script>";
+					$data = mysqli_fetch_array($result);
+					$data = $data['name'];
+					if(empty($data)){
+						if($pass == $pass_repeat){
+							$query = "SELECT * FROM users WHERE email = '$email'";
+							$result = mysqli_query($connection, $query);
+							if(!empty($result) || empty($email)){
+								$query = "INSERT INTO users (name, password, email) VALUES ('$name', '$pass', '$email')";
+								$result = mysqli_query($connection, $query);
+								if($result){
+										echo "Byl/a jsi úspěšně zaregistrován/a.";
+										$_SESSION['user']=$name;
+										filelog("Uživatel $name se zaregistroval do systému.");
+										echo "<script type='text/javascript'>location.href = 'home.php';</script>";
+								}
+								else{echo "<div id='registerError'>Nastala chyba. Zkuste to později, nebo kontaktujte administrátora na emailu honza.stech@gmail.com."; echo mysqli_error($connection)."</div>";}
+							}
+							else{echo "<div id='registerError'>Uživatel s tímto e-mailem již existuje.</div>";}
 						}
-						else{echo "Nastala chyba. Zkuste to později, nebo kontaktujte administrátora na emailu honza.stech@gmail.com.<br />";echo mysqli_error($connection);}
+						else{echo "<div id='registerError'>Hesla se neschodují.</div>";}
 					}
-					else{echo "Uživatel s tímto e-mailem již existuje.";}
+					else{echo "<div id='registerError'>Uživatel s tímto jménem již existuje.</div>";}
 				}
-				else{echo "Hesla se neschodují.";}
+				else{echo "<div id='registerError'>Musíš udělit souhlas se spracováním zadaných údajů.</div>";}
 			}
-			else{echo "Uživatel s tímto jménem již existuje.";}
-		}
-		else{echo "Musíš udělit souhlas se spracováním zadaných údajů.";}
-	}
-	mysqli_close($connection);
-?>
-<p>Jsi již zaregistrován? Přihlaš se <a href="login.php"><u>zde</u></a>.</p>
+			mysqli_close($connection);
+		?>
+	</div>
+</body>
