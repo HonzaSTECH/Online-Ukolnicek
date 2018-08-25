3ec41251241272c73f2ac0a69362175bfe4380f9
BuildTools <unconfigured@null.spigotmc.org>
2018-08-25 16:14:28 +0200

    TODO.txt file has been added and contains list of things to do. NewClassBox and ApplyBox elements on home page has been added transitions (grows when hovered over them). Info tab now works and points to new info page. The info page doesn´t contain almost any informations yet. Radio buttons on home page are bigger now.

---
+++ scripts/info.php
@@ -0,0 +1,20 @@
+<?php
+	session_start(); 
+	require 'checker.php';
+	check(true);
+?>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="../styles/info.css">
+</head>
+<body>
+	<span>
+		<h1>O aplikaci</h1>
+		<br />
+		Tvůrce: Jan Štěch
+		<br />
+		Vytvořeno v roce 2018
+	</span>
+	<br />
+	<a href="home.php">Návrat</a>
+</body>
