d0a32af4f8ca61a53858bb3c0dac56d6acafec2d
BuildTools <unconfigured@null.spigotmc.org>
2018-09-02 19:02:12 +0200

    Customizing list of class's subjects is now possible on classManagement page. Howerver some styling of the page is still required. On classManagement page, you can see list of class' members / moderators / admins. Some action buttons (Rank, Kick, etc.) will be added in the future.

---
+++ scripts/classManagement.js
@@ -0,0 +1,34 @@
+function firstTab(){
+	document.getElementById("tab2").style.display = "none";
+	document.getElementById("tab3").style.display = "none";
+	document.getElementById("tab4").style.display = "none";
+	
+	document.getElementById("tab1").style.display = "block";
+}
+function secondTab(){
+	document.getElementById("tab1").style.display = "none";
+	document.getElementById("tab3").style.display = "none";
+	document.getElementById("tab4").style.display = "none";
+	
+	document.getElementById("tab2").style.display = "block";
+}
+function thirdTab(){
+	document.getElementById("tab1").style.display = "none";
+	document.getElementById("tab2").style.display = "none";
+	document.getElementById("tab4").style.display = "none";
+	
+	document.getElementById("tab3").style.display = "block";
+}
+function fourthTab(){
+	document.getElementById("tab1").style.display = "none";
+	document.getElementById("tab2").style.display = "none";
+	document.getElementById("tab3").style.display = "none";
+	
+	document.getElementById("tab4").style.display = "block";
+}
+function changeSubjects(){
+	document.getElementById("subjectsForm").style.display = "block";
+}
+function hideForm(){
+	document.getElementById("subjectsForm").style.display = "none";
+}
