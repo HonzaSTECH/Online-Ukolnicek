b24da5fa2e73256866b4c6f548af26d174188fb7
BuildTools <unconfigured@null.spigotmc.org>
2018-09-03 17:11:05 +0200

    Registration now checks for multiple conditions. Name length, password length, passwork strength, email length and so on. Registration file doesn´t contains so much nested if / else statements as before.

--- scripts/register.php
+++ scripts/register.php
@@ -22,40 +22,54 @@
 			require_once("connect.php");
 			include 'logger.php';
 			
-			if(isset($_POST['send'])){
+			if(isset($_POST['send']))
+			{
 				$name = $_POST['username'];
 				$pass = $_POST['password'];
 				$pass_repeat = $_POST['password_repeat'];
 				$email = $_POST['email'];
 				$accept = @$_POST['accept'];
 				
-				if(isset($accept)){
+				if(!isset($accept)){echo "<div id='registerError'>Musíš udělit souhlas se spracováním zadaných údajů.</div>";}
+				else		//Accepted terms
+				{
 					$query = "SELECT * FROM users WHERE name='$name'";
 					$result = mysqli_query($connection, $query);
 					$data = mysqli_fetch_array($result);
 					$data = $data['name'];
-					if(empty($data)){
-						if($pass == $pass_repeat){
-							$query = "SELECT * FROM users WHERE email = '$email'";
+					if(strlen($name) < 4){echo "<div id='registerError'>Jméno musí být alespoň 4 znaky dlouhé.</div>";}
+					else if(strlen($name) > 16){echo "<div id='registerError'>Jméno nesmí být více než 16 znaků dlouhé.</div>";}
+					else if(!empty($data)){echo "<div id='registerError'>Uživatel s tímto jménem již existuje.</div>";}
+					else if(strlen($pass) < 6){echo "<div id='registerError'>Heslo musí být alespoň 6 znaků dlouhé.</div>";}
+					else if(strlen($pass) > 32){echo "<div id='registerError'>Heslo nesmí být více než 32 znaků dlouhé.</div>";}
+					else if($pass != $pass_repeat){echo "<div id='registerError'>Hesla se neschodují.</div>";}
+					else if(!preg_match("#[0-9]+#", $pass)){echo "<div id='registerError'>Heslo musí obsahovat alespoň jednu číslici.</div>";}
+					else if(!preg_match("#[a-z]+#", $pass)){echo "<div id='registerError'>Heslo musí obsahovat alespoň jedno malé písmeno</div>";}
+					else if(!preg_match("#[A-Z]+#", $pass)){echo "<div id='registerError'>Heslo musí obsahovat alespoň jedno velké písmeno</div>";}
+					else		//Valid name and password
+					{
+						$query = "SELECT * FROM users WHERE email = '$email'";
+						$result = mysqli_query($connection, $query);
+						$data = mysqli_fetch_array($result);
+						$data = $data['email'];
+						if(!filter_var($email, FILTER_VALIDATE_EMAIL)){echo "<div id='registerError'>Musíš zadat platnou e-mailovou adresu.</div>";}
+						else if(strlen($email) > 64){echo "<div id='registerError'>E-mail nesmí být více než 64 znaků dlouhé</div>";}
+						else if(!empty($data)){echo "<div id='registerError'>Uživatel s tímto e-mailem již existuje.</div>";}
+						else		//Valid e-mail
+						{
+							$query = "INSERT INTO users (name, password, email) VALUES ('$name', '$pass', '$email')";
 							$result = mysqli_query($connection, $query);
-							if(!empty($result) || empty($email)){
-								$query = "INSERT INTO users (name, password, email) VALUES ('$name', '$pass', '$email')";
-								$result = mysqli_query($connection, $query);
-								if($result){
-										echo "<div id='successMessage'>Byl/a jsi úspěšně zaregistrován/a.</div>";
-										$_SESSION['user']=$name;
-										filelog("Uživatel $name se zaregistroval do systému.");
-										echo "<script type='text/javascript'>location.href = 'home.php';</script>";
-								}
-								else{echo "<div id='registerError'>Nastala chyba. Zkuste to později, nebo kontaktujte administrátora na emailu honza.stech@gmail.com."; echo mysqli_error($connection)."</div>";}
+							if(!$result){echo "<div id='registerError'>Nastala chyba. Zkuste to později, nebo kontaktujte administrátora na e-mailu honza.stech@gmail.com."; echo mysqli_error($connection)."</div>";}
+							else		//Registred
+							{
+								echo "<div id='successMessage'>Byl/a jsi úspěšně zaregistrován/a.</div>";
+								$_SESSION['user']=$name;
+								filelog("Uživatel $name se zaregistroval do systému.");
+								echo "<script type='text/javascript'>location.href = 'home.php';</script>";
 							}
-							else{echo "<div id='registerError'>Uživatel s tímto e-mailem již existuje.</div>";}
 						}
-						else{echo "<div id='registerError'>Hesla se neschodují.</div>";}
 					}
-					else{echo "<div id='registerError'>Uživatel s tímto jménem již existuje.</div>";}
 				}
-				else{echo "<div id='registerError'>Musíš udělit souhlas se spracováním zadaných údajů.</div>";}
 			}
 			mysqli_close($connection);
 		?>
