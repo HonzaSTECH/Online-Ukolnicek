ca6e34463a236e17a93836402be89de6d4a60a07
BuildTools <unconfigured@null.spigotmc.org>
2018-09-18 17:05:16 +0200

    Adding new records work again using AJAX. New record is added and displayed with no need to reload the page from now.

--- scripts/AJAXnewRecord.php
+++ scripts/AJAXnewRecord.php
@@ -1,39 +1,30 @@
 <?php
-    require_once('connect.php');
+	session_start();
+	require_once('connect.php');
 	include 'logger.php';
 	
-	if(isset($_POST['send'])){
+	$a = $_COOKIE['date'];
+	$b = $_COOKIE['subject'];
+	$c = $_COOKIE['description'];
+	$d = $_SESSION['user'];
+	$e = $_COOKIE['priority'];
 	
-		$a = $_POST['date'];
-		$b = $_POST['subject'];
-		$c = $_POST['description'];
-		$d = $_SESSION['user'];
-		$e = $_POST['priority'];
-	/*
-		echo $a."<br />";
-		echo $b."<br />";
-		echo $c."<br />";
-		echo $d."<br />";
-		echo $e."<br />";
-	*/	
-		$query = "INSERT INTO records (date, subject, description, author, dateOfAdding, priority, class) values ('$a', '$b', '$c', '$d', NOW(), '$e', '$classId')";
+	$classId = $_SESSION['class'];
+
+	$query = "INSERT INTO records (date, subject, description, author, dateOfAdding, priority, class) values ('$a', '$b', '$c', '$d', NOW(), '$e', '$classId')";
+	$result = mysqli_query($connection, $query);
+	if ($result)
+	{
+		$user = $_SESSION['user'];
+		$query = "SELECT name FROM classes WHERE id='$classId'";
 		$result = mysqli_query($connection, $query);
-		if ($result){
-			/*echo "
-				<script>
-					document.getElementsByClassName('action1')[recordCount].onclick = upvoteRecord;
-					document.getElementsByClassName('action2')[recordCount].onclick = editRecord;
-					document.getElementsByClassName('action3')[recordCount].onclick = removeRecord;
-				</script>";
-			*/
-				$user = $_SESSION['user'];
-				fileLog("Uživatel $user přidal ve třídě $name záznam: $a - $b - $c - $e. priorita");
-				//echo "<script type='text/javascript'>location.href = 'list.php';</script>";
-		}
-		else{
-            echo "An error occured: ".mysqli_error($connection);
-            mysqli_close($connection);
-        }
+		$result = mysqli_fetch_array($result);
+		$name = $result['name'];
+		fileLog("Uživatel $user přidal ve třídě $name záznam: $a - $b - $c - $e. priorita");
 	}
-    else{mysqli_close($connection);}
+	else
+	{
+		echo "An error occured: ".mysqli_error($connection);
+		mysqli_close($connection);
+        }
 ?>
