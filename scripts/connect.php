20ae0aa3114c377310850f3c18024462db5f8928
Jan Štěch <honza.stech@gmail.com>
2018-10-08 19:26:29 +0200

    Secured classManagement page. When a user tries to access the classManagement page by typing .../classManagement.php into URL box and he is not an admin, he is automaticly redirected back to the list page. A few bugs with including/requiring checker.php and connect.php scripts are fixed.

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,11 +1,10 @@
 <?php
-	define('host', "185.64.219.6:3306");
-	define('username', "seznamtestuc3809");
-	define('password', "hidden");
-	define('database', "seznamtestuc3809");
+	@define('host', "185.64.219.6:3306");
+	@define('username', "seznamtestuc3809");
+	@define('password', "hidden");
+	@define('database', "seznamtestuc3809");
 	$data = "";
 	$result = "";
-	
 	//Connect to the database
 	$connection = @mysqli_connect(host, username, password, database) OR die("Connection to the database failed. Error: ".mysqli_connect_error);
         //echo "Succefully connected to SQL database.<br />";
