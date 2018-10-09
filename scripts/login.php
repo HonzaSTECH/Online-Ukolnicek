089c320a173d360655dca736c78f4946a91920a0
Jan Štěch <honza.stech@gmail.com>
2018-10-09 16:23:21 +0200

    Login and register pages are commented.

--- scripts/login.php
+++ scripts/login.php
@@ -18,37 +18,59 @@
 			<input type=submit name="send" value="Přihlásit se" id="submitButton">
 		</form>
 		<div id="registerLink">Ještě nemáš účet? Zaregistruj se <a href="register.php"><u>zde</u></a>.</div>
+		
 		<?php
 			require_once("connect.php");
 			include 'logger.php';
 			
-			if(isset($_POST['send'])){
+			//Handling form submit
+			if(isset($_POST['send']))
+			{
+				//Getting inputed data
 				$name = $_POST['username'];
 				$pass = $_POST['password'];
 				
+				//Getting user's account details from the database
 				$query = "SELECT * FROM users WHERE name='$name'";
 				$result = mysqli_query($connection, $query);
-				
 				$data = mysqli_fetch_array($result);
 				
-				if($data['name'] == $name){
-					if(password_verify($pass, $data['password'])){
+				//Checking for existence of a account with given name
+				if($data['name'] == $name)
+				{
+					//Checking for correct password
+					if(password_verify($pass, $data['password']))
+					{
+						//Displaying success message
 						echo "<div id='successMessage'>Byl/a jsi úspěšně přihlášen/a.</div>";
+						
+						//Saving user's name into superglobal
 						$_SESSION['user']=$name;
+						
+						//Logging the signing in from a IP adress
 						$ip = $_SERVER['REMOTE_ADDR'];
 						fileLog("Uživatel $name se přihlásil z IP adresy $ip.");
+						
+						//Redirecting
 						echo "<script type='text/javascript'>location.href = 'home.php';</script>";
 					}
 					else{
+						//Displaying error message
 						echo "<div id='loginError'>Špatné heslo</div>";
+						
+						//Logging the attempt
 						$ip = $_SERVER['REMOTE_ADDR'];
 						fileLog("Z adresy $ip proběhl pokus o přihlášení k účtu $name.");
 						}
 				}
-				else{echo "<div id='loginError'>Uživatel s tímto jménem neexistuje.</div>";}
+				else
+				{
+					//Displaying error message
+					echo "<div id='loginError'>Uživatel s tímto jménem neexistuje.</div>";
+				}
 			}
 			mysqli_close($connection);
-
 		?>
-        </div>
+	
+	</div>
 </body>
--- scripts/register.php
+++ scripts/register.php
@@ -3,6 +3,7 @@
 	unset($_SESSION['class']);
 	unset($_SESSION['user']);
 ?>
+
 <head>
 	<meta charset="utf-8">
 	<link rel="shortcut icon" href="../images/marklessLogo.ico">
@@ -19,28 +20,37 @@
 			<input type="submit" name="send" value="Zaregistrovat se" id="submitButton">
 		</form>
 		<div id="loginLink">Jsi již zaregistrován? Přihlaš se <a href="login.php"><u>zde</u></a>.</div>
+		
 		<?php
 			require_once("connect.php");
 			include 'logger.php';
 			
+			//Handling form submit
 			if(isset($_POST['send']))
 			{
+				//Getting inputed data
 				$name = $_POST['username'];
 				$pass = $_POST['password'];
 				$pass_repeat = $_POST['password_repeat'];
 				$email = $_POST['email'];
 				$accept = @$_POST['accept'];
 				
+				//Checking for acceptence of terms of service
 				if(!isset($accept)){echo "<div id='registerError'>Musíš udělit souhlas se spracováním zadaných údajů.</div>";}
 				else		//Accepted terms
 				{
+					//Searching the database for an existing account with the same name
 					$query = "SELECT * FROM users WHERE name='$name'";
 					$result = mysqli_query($connection, $query);
 					$data = mysqli_fetch_array($result);
 					$data = $data['name'];
+					
+					//Checking for valid username
 					if(strlen($name) < 4){echo "<div id='registerError'>Jméno musí být alespoň 4 znaky dlouhé.</div>";}
 					else if(strlen($name) > 16){echo "<div id='registerError'>Jméno nesmí být více než 16 znaků dlouhé.</div>";}
 					else if(!empty($data)){echo "<div id='registerError'>Uživatel s tímto jménem již existuje.</div>";}
+					
+					//Checking for valid password
 					else if(strlen($pass) < 6){echo "<div id='registerError'>Heslo musí být alespoň 6 znaků dlouhé.</div>";}
 					else if(strlen($pass) > 32){echo "<div id='registerError'>Heslo nesmí být více než 32 znaků dlouhé.</div>";}
 					else if($pass != $pass_repeat){echo "<div id='registerError'>Hesla se neschodují.</div>";}
@@ -49,24 +59,38 @@
 					else if(!preg_match("#[A-Z]+#", $pass)){echo "<div id='registerError'>Heslo musí obsahovat alespoň jedno velké písmeno</div>";}
 					else		//Valid name and password
 					{
+						//Searching the database for an existing account with the same e-mail
 						$query = "SELECT * FROM users WHERE email = '$email'";
 						$result = mysqli_query($connection, $query);
 						$data = mysqli_fetch_array($result);
 						$data = $data['email'];
+						
+						//Checking for valid e-mail address
 						if(!filter_var($email, FILTER_VALIDATE_EMAIL)){echo "<div id='registerError'>Musíš zadat platnou e-mailovou adresu.</div>";}
-						else if(strlen($email) > 64){echo "<div id='registerError'>E-mail nesmí být více než 64 znaků dlouhé</div>";}
+						else if(strlen($email) > 64){echo "<div id='registerError'>E-mail nesmí být více než 64 znaků dlouhý</div>";}
 						else if(!empty($data)){echo "<div id='registerError'>Uživatel s tímto e-mailem již existuje.</div>";}
 						else		//Valid e-mail
 						{
+							//Encrypting the password
 							$pass = password_hash($pass, PASSWORD_DEFAULT);
+							
+							//Saving the account into the database
 							$query = "INSERT INTO users (name, password, email) VALUES ('$name', '$pass', '$email')";
 							$result = mysqli_query($connection, $query);
+							
+							//Checking for error
 							if(!$result){echo "<div id='registerError'>Nastala chyba. Zkuste to později, nebo kontaktujte administrátora na e-mailu honza.stech@gmail.com."; echo mysqli_error($connection)."</div>";}
 							else		//Registred
 							{
+								//Displaying success message
 								echo "<div id='successMessage'>Byl/a jsi úspěšně zaregistrován/a.</div>";
+								
+								//Saving user's name into superglobal
 								$_SESSION['user']=$name;
-								filelog("Uživatel $name se zaregistroval do systému.");
+								
+								//Logging the account creation
+								$ip = $_SERVER['REMOTE_ADDR'];
+								filelog("Uživatel $name se zaregistroval do systému z IP adresy $ip.");
 								echo "<script type='text/javascript'>location.href = 'home.php';</script>";
 							}
 						}
@@ -75,5 +99,6 @@
 			}
 			mysqli_close($connection);
 		?>
+		
 	</div>
 </body>
