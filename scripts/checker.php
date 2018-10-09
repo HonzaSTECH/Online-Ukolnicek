487aaeba44988f06356f5394e4d1c9c917168efc
Jan Štěch <honza.stech@gmail.com>
2018-10-09 15:15:02 +0200

    Helper files are commented. That includes checker.php , classLogin.php , connect.php and logger.php .todo updated

--- scripts/checker.php
+++ scripts/checker.php
@@ -1,23 +1,33 @@
 <?php
-	function check($check_user = false, $check_class = false, $check_admin = false){
-		if($check_user){
-			if(!isset($_SESSION['user'])){
+	function check($check_user = false, $check_class = false, $check_admin = false)
+	{
+		if($check_user)		//Checking for logged user
+		{
+			if(!isset($_SESSION['user']))
+			{
 				echo "<script type='text/javascript'>location.href = 'login.php';</script>";
 			}
 		}
-		if($check_class){
-			if(!isset($_SESSION['class'])){
+		if($check_class)		//Checking for entered class
+		{
+			if(!isset($_SESSION['class']))
+			{
 				echo "<script type='text/javascript'>location.href = 'home.php';</script>";
 			}
 		}
-		if($check_admin){
+		if($check_admin)	//Checking for being admin of the entered class
+		{
+			//Getting admin of the entered class
 			require_once 'connect.php';
 			$classId = $_SESSION['class'];
-			$username = $_SESSION['user'];
 			$query = "SELECT admin FROM classes WHERE id='$classId'";
 			$result = mysqli_query($connection, $query);
 			$result = mysqli_fetch_array($result);
 			$result = $result['admin'];	
+			
+			$username = $_SESSION['user'];
+			
+			//Comparing admin name with logged user's name
 			if($username != $result)
 			{
 				echo "<script type='text/javascript'>location.href = 'list.php';</script>";
--- scripts/connect.php
+++ scripts/connect.php
@@ -1,13 +1,13 @@
 <?php
+	//Define the connection constatns
 	@define('host', "185.64.219.6:3306");
 	@define('username', "seznamtestuc3809");
 	@define('password', "hidden");
 	@define('database', "seznamtestuc3809");
-	$data = "";
-	$result = "";
+	
 	//Connect to the database
 	$connection = @mysqli_connect(host, username, password, database) OR die("Connection to the database failed. Error: ".mysqli_connect_error);
-        //echo "Succefully connected to SQL database.<br />";
+	
+	//Setting charset of the connection
 	mysqli_set_charset($connection, 'utf8') OR die("Couldn´t set the charset to utf8");
-        //echo "Succefully set charset to uth8mb4<br />";
 ?>
