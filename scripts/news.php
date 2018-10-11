1f0170dba92283e1de39c37f0e305ba6f1011337
Jan Štěch <honza.stech@gmail.com>
2018-10-11 17:55:41 +0200

    The news.php file now works properly on both index and info pages.

--- scripts/news.php
+++ scripts/news.php
@@ -1,4 +1,6 @@
-<!--	  TODO		-->
 <?php
-	echo "<span>NOVINKY</span>"
+	if ($_SERVER['SCRIPT_FILENAME'] == "/3w/chytrak.cz/s/seznamtestu/index.php"){$read = file("news.txt");}
+	else{$read = file("../news.txt");}
+	
+	foreach ($read as $line){echo $line."<br />";}
 ?>
