1869a6048fdb83e4454f51ac0007091737ea6ec6
Jan Štěch <honza.stech@gmail.com>
2018-11-04 14:37:41 +0100

    Shortcut icon on the index page is fixed.

--- scripts/index.php
+++ scripts/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="images/transparentMarklessLogo.png">
+	<link rel="shortcut icon" href="images/transparentMarklessLogo.ico">
 	<link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
