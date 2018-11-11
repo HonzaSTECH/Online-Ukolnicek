900ca2a83470aed9e9ab3db4d684afbe30e93774
Jan Štěch <honza.stech@gmail.com>
2018-11-11 16:42:15 +0100

    The apply page is definitely styled. The applyForm page is merged into the apply page. .gitignore updated .todo uptated The "Member in no classes" text is positioned better on the home page.

--- scripts/apply.php
+++ scripts/apply.php
@@ -9,47 +9,211 @@
 	<link rel="stylesheet" href="../styles/apply.css">
 </head>
 <body>
-	<p>Seznam tříd:</p>
-	
-	<!-- Getting and writing classes the user isn't member in -->
-	<?php
-		require_once("connect.php");
-		
-		$user = $_SESSION['user'];
-		$query = "SELECT memberIn FROM users WHERE name='$user'";
-		$result = mysqli_query($connection, $query);
-		$alreadyMemberIn = mysqli_fetch_array($result);
-		$alreadyMemberIn = $alreadyMemberIn['memberIn'];
-		if(!$alreadyMemberIn == 0){$alreadyMemberIn = explode(',',$alreadyMemberIn);}
-		else{$alreadyMemberIn = array(0);}
-		$count = count($alreadyMemberIn);
-		$query = "SELECT * FROM classes WHERE id NOT IN (";
-		foreach($alreadyMemberIn as $class)
-		{
-			$count--;
-			$query .= $class;
-			if($count > 0){$query .= ", ";}
-			else{$query .= ");";}
-		}
-		
-		$result = mysqli_query($connection, $query);
+	<div id="header">
+		<span id="username">
+			<?php
+			echo "Jsi přihlášen jako ";
+			echo $_SESSION['user'];
+			?>
+		</span>
+		<a href="login.php">
+			<div id="logoutBox">
+				<span id="logoutLink">Odhlásit se</span>
+			</div>
+		</a>
+		<a href="info.php">
+			<div id="infoBox">
+				<span id="infoLink">Informace</span>
+			</div>
+		</a>
+		<a href="home.php">
+			<div id="homeBox">
+				<span id="homeLink">Domů</span>
+			</div>
+		</a>
+	</div>
+	<div id="container">
+		<div id="infoText">
+			<h2>Zažádat o přijetí do existující třídy</h2>
+			<span id=subtext>
+				Vyber si třídu do které se chceš přidat a zadej osobní údaje jako informace pro adminitrátora nebo moderátory třídy kteří budou muset tvojí žádost o přijetí potvrdit.
+			</span>
+		</div>
+		<form action="apply.php" method="POST">
+			<fieldset id=classSelect>
+				<?php	//Printing list of classes to join 
+					require_once("connect.php");
+					
+					//Getting names of all avilibe classes from the database
+					$user = $_SESSION['user'];
+					$query = "SELECT memberIn FROM users WHERE name='$user'";
+					$result = mysqli_query($connection, $query);
+					$alreadyMemberIn = mysqli_fetch_array($result);
+					$alreadyMemberIn = $alreadyMemberIn['memberIn'];
+					if(!$alreadyMemberIn == 0){$alreadyMemberIn = explode(',',$alreadyMemberIn);}
+					else{$alreadyMemberIn = array(0);}
+					$count = count($alreadyMemberIn);
+					$query = "SELECT name FROM classes WHERE id NOT IN (";
+					foreach($alreadyMemberIn as $class)
+					{
+						$count--;
+						$query .= $class;
+						if($count > 0){$query .= ", ";}
+						else{$query .= ");";}
+					}
+					$result = mysqli_query($connection, $query);
+					
+					//Getting amount of needed columns for the table
+					$count = mysqli_num_rows($result);
+					if($count == 0){$columns = 0;}
+					else if($count <= 4){$columns = 1;}
+					else if($count <= 8){$columns = 2;}
+					else if($count <= 12){$columns = 3;}
+					else if($count > 12){$columns = 4;}
+					
+					if($columns == 0){echo "Jsi již členem ve všech existujících třídách.";}
+					
+					//Getting amount of needed rows for the table
+					if($columns <= 1){$rowCount = $count;}
+					else if($columns <= 3){$rowCount = ceil($count / $columns);}
+					else{$rowCount = ceil($count / 3);}
+					
+					//Setting width for each column depending on columns count
+					switch($columns)
+					{
+						case 1:
+							$width = 24;
+							break;
+						case 2:
+							$width = 12;
+							break;
+						case 3:
+							$width = 8;
+							break;
+						case 4:
+							$width = 8;
+							break;
+						default:
+							//No available classes
+							$width = 0;
+							break;
+					}
+					
+					//Getting names of all avalibe classes in to numeric array
+					$classes = array();
+					while($row = mysqli_fetch_array($result))
+					{
+						$name = $row['name'];
+						array_push($classes, $name);
+					}
+					//Creating rows of the table and printing it
+					echo "<table>";
+					
+					for($i = 0; $i < $rowCount; $i++)
+					{
+						$row = array();
+						for($j = 0; $j < $columns; $j++)
+						{
+							$index = ($rowCount * $j) + $i;
+							if(!isset($classes[$index])){break;}
+							array_push($row, $classes[$index]);
+						}
+						//Printing
+						echo "<tr>";
+							for($k = 0; $k < $columns; $k++)
+							{
+								if(!isset($row[$k])){break;}
+								$classname = $row[$k];
+								echo "<td style='width:".$width."vw; min-width:".$width."vw; max-width:".$width."vw;'><label>";
+									echo"<input type=radio name='applyTo' value='$classname' required>$classname";
+								echo "</label></td>";
+							}
+						echo "</tr>";
+					}
+					echo "</table>";
+				?>
+			</fieldset>
+			<fieldset>
+				<input type=text name="name" placeholder="Jméno" id="name" required>
+				<input type=text name="surname" placeholder="Přijímění" id="surname" required>
+				<textarea type="message" name="message" placeholder="Dodatečné informace" id="text" required>Ahoj. Rád bych se přidal do vaší třídy. Přijmete prosím mojí žádost?</textarea>
+				<input type=submit name="posted" value="Odeslat žádost" id="submitButton" <?php	if($count <= 0){echo "disabled";}	?>>
+				</fieldset>
+		</form>
 
-		echo "<form method='POST' action='apply.php'><table>";
-		while($row = mysqli_fetch_array($result))
-		{
-			$name = $row['name'];
-			echo "<tr><td align='center' class='td1'><input type=radio name='applyTo' value='$name'></td><td align='center' class='td2'>$name</td></tr>";
-		}
-		echo "</table><input type=submit name='apply' value='Zažádat o přijetí'></form>"
-	?>
+		<?php
+			//require_once("connect.php");
+			include 'logger.php';
 
-	<a href="home.php">Návrat na seznam tříd</a>
+			if(isset($_POST['posted'])){
+				
+				//Getting ID of the class
+				$className = $_POST['applyTo'];
+				$query = "SELECT id FROM classes WHERE name='$className'";
+				$result = mysqli_query($connection, $query);
+				$result = mysqli_fetch_array($result);
+				$class = $result['id'];
+				unset($_SESSION['applyClass']);
+				
+				//Getting e-mail of the admin of the class
+				$query = "SELECT admin FROM classes WHERE id='$class'";
+				$result = mysqli_query($connection, $query);
+				if (!$result){echo "An error occured. Error: ".mysqli_error();}
+				$admin = mysqli_fetch_array($result);
+				$admin = $admin['admin'];
+				$query = "SELECT email FROM users WHERE name='$admin'";
+				$result = mysqli_query($connection, $query);
+				if (!$result){echo "An error occured. Error: ".mysqli_error();}
+				$email = mysqli_fetch_array($result);
+				$email = $email['email'];
+
+				//Getting application details
+				$user = $_SESSION['user'];
+				$name = $_POST['name'];
+				$surname = $_POST['surname'];
+				$message = $_POST['message'];
+				
+				//Replacing linebreaks with <br>
+				$message = str_replace(array("\r\n"), '<br>', $message);
+				
+				//Writing the application into the database
+				$timestamp = time();
+				$query = "INSERT INTO applications (nickname, name, surname, message, class, age) VALUES ('$user', '$name', '$surname', '$message', '$class', '$timestamp')";
+				$result = mysqli_query($connection, $query);
+				
+				//Checking for error
+				if(!$result){echo "An Error occured. Error: ".mysqli_error($connection);}
+				
+				//Building e-mail for the admin
+				//str_replace("<br>", "\n", $message);
+				$message = wordwrap($message, 70, "<br />");
+				
+				$email_subject = "Žádost o přijetí do třídy $className od: $name $surname";
+				//Building e-mail body
+				$email_body = "<h2>Detaily žádosti:</h2>".
+				"<b>Jméno:</b> $name".
+				"<br /><b>Přijímení:</b> $surname".
+				"<br /><b>Text žádosti:</b>".
+				"<br />$message".
+				"<br /><br />".
+				"<br /><i>Žádost můžete přijmout nebo zamítnout na stránce se seznamem testů.".
+				"<br />Tuto Žadost schvalte pouze v případě, že jste si jistí kdo tento uživatel ve skutečnosti je.".
+				"<br />Tento e-mail byl vygenerován automaticky a tudíž na něj neodpovídejte.</i>";
+				
+				$headers = "From: Seznam testů info <info@seznamtestu.chytrak.cz>\n";
+				$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+				
+				//Sending e-mail to the admin of the class
+				mail($email,$email_subject,$email_body,$headers);
+				
+				//Logging the application
+				$user = $_SESSION['user'];
+				fileLog("Uživatel $user zažádal o přijetí do třídy $class");
+				
+				//Redirecting to the home page
+				echo "<script>alert('Vaše žádost o přijetí do této třídy byla odeslána. O přijetí nebo zamítnutí požadavku se dozvíte na stránce se seznamem tříd.');</script>";
+				echo "<script type='text/javascript'>location.href = 'home.php';</script>";
+			}
+		?>
+	</div>
 </body>
-<?php
-	//Handling form submit
-	if(isset($_POST['apply']))
-	{
-		$_SESSION['applyClass'] = $_POST['applyTo'];
-		echo "<script type='text/javascript'>location.href = 'applyForm.php';</script>";
-	}
-?>
