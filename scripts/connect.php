7a02ad1a17a45662a31ae49a981ac9fa88b031c8
Jan Štěch <honza.stech@gmail.com>
2019-02-22 17:58:04 +0100

    The description text on the class management page is vertically aligned to middle again. However the horizontal scrollbar will no longer appear in the bottom of the table cell if needed. connect.php file now contains correct data for logging into the database.

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,9 +1,9 @@
 <?php
 	//Define the connection constatns
-	@define('host', "185.64.219.6:3306");
-	@define('username', "seznamtestuc3809");
-	@define('password', "MgCa(CO3)2");
-	@define('database', "seznamtestuc3809");
+	@define('host', "localhost");
+	@define('username', "u203826608_admin");
+	@define('password', "DsTvgBt6UCovLkTp");
+	@define('database', "u203826608_exams");
 	
 	//Connect to the database
 	$connection = @mysqli_connect(host, username, password, database) OR die("Connection to the database failed. Error: ".mysqli_connect_error);
