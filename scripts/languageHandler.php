688c58ee45498c6045d28ff680c7275cd8862428
Jan Štěch <honza.stech@gmail.com>
2019-02-28 07:50:05 +0100

    Czech is now added into the languageHandler.php file as a valid language. In case the selected language is unavalible, English is used instead of no text.

--- scripts/languageHandler.php
+++ scripts/languageHandler.php
@@ -7,25 +7,25 @@
 	$urlExtension = "?lang=$language";
 	
 	
-	$avalibeLanguages = array('en','pl', 'cn');
+	$avalibeLanguages = array('en','pl','cn','cs');
 	
-	if (in_array($language,$avalibeLanguages))
+	if (!in_array($language,$avalibeLanguages))
 	{
-		if ($_SERVER['SCRIPT_FILENAME'] == "/home/u203826608/domains/examdirectory.online/public_html/index.php")
-		{
-			$langFile = "lang/$language.php";
-		}
-		else
-		{
-			$langFile = "../lang/$language.php";
-		}
-		include "$langFile";
-		
-		/*$_SESSION['langTemp'] = $language;
-		echo "<script>console.log('Selected language: ".$_SESSION['langTemp']."');</script>"; //For debug reasons only*/
+		echo "Selected language is unavalible";
+		$language = 'en';
+	}
+	
+	if ($_SERVER['SCRIPT_FILENAME'] == "/home/u203826608/domains/examdirectory.online/public_html/index.php")
+	{
+		$langFile = "lang/$language.php";
 	}
 	else
 	{
-		echo "Selected language is unavalible";
+		$langFile = "../lang/$language.php";
 	}
+	include "$langFile";
+	
+	echo "<script>console.log('Selected language: ".$_SESSION['langTemp']."');</script>"; //For debug reasons only*/
+}
+	
 ?>
